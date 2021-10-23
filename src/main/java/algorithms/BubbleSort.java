c77e07cdb8bf6884b53380ca656fabb72e7578a2
Adam Jonas <adam.jonas@ppb.com>
2021-10-23 23:39:25 +0100

    Refactor bubble sort again

--- src/main/java/algorithms/BubbleSort.java
+++ src/main/java/algorithms/BubbleSort.java
@@ -8,15 +8,12 @@ public class BubbleSort {
         while (true) {
             int counter = 0;
             for (int i = 0; i < input.size() -1; i++) {
-                Integer iInt = input.get(i);
-                Integer jInt = input.get(i + 1);
-                if (iInt > jInt) {
+                if (input.get(i) > input.get(i + 1)) {
                     Collections.swap(input, i, i + 1);
                     counter++;
                 }
             }
-            if (counter == 0) break;
+            if (counter == 0) return input;
         }
-        return input;
     }
 }
