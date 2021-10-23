719c6bc987c7baee49321c7aa2fb9772658a8fda
Adam Jonas <adam.jonas@ppb.com>
2021-10-23 19:51:35 +0100

    Implement find duplicates algorithm

--- src/main/java/algorithms/FindDuplicates.java
+++ src/main/java/algorithms/FindDuplicates.java
@@ -1,11 +1,21 @@
 package algorithms;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class FindDuplicates {
     public <T> Set<T> findDuplicates(List<T> input) {
-        return Collections.emptySet();
+        HashSet<T> set = new HashSet<>();
+        HashSet<T> output = new HashSet<>();
+
+        for (T element : input) {
+            if (!set.add(element)) {
+                output.add(element);
+            }
+        }
+
+        return output;
     }
 }
--- src/test/java/algorithms/FindDuplicatesTest.java
+++ src/test/java/algorithms/FindDuplicatesTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Set;
 
 import static java.util.Collections.emptyList;
 import static java.util.Collections.emptySet;
@@ -13,13 +14,31 @@ public class FindDuplicatesTest {
     private FindDuplicates victim;
 
     @BeforeEach
-    private void setUp() {
+    public void setUp() {
         victim = new FindDuplicates();
     }
 
     @Test
     public void findDuplicates_emptyList() {
         List<Integer> input = emptyList();
-        assertEquals(victim.findDuplicates(input), emptySet());
+        assertEquals(emptySet(), victim.findDuplicates(input));
+    }
+
+    @Test
+    public void findDuplicates_oneDuplicate() {
+        List<Integer> input = List.of(1, 1);
+        assertEquals(Set.of(1), victim.findDuplicates(input));
+    }
+
+    @Test
+    public void findDuplicates_manyDuplicates() {
+        List<String> input = List.of("1", "0", "2", "4", "7", "1", "5", "5");
+        assertEquals(Set.of("1", "5"), victim.findDuplicates(input));
+    }
+
+    @Test
+    public void findDuplicates_noDuplicates() {
+        List<String> input = List.of("1", "0", "2", "4", "7", "3", "5", "9");
+        assertEquals(emptySet(), victim.findDuplicates(input));
     }
 }
