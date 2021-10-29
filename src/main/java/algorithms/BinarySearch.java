a16d8bb61d4a3d5dc1218bcdc87289e7d13f806b
Adam Jonas <adam.jonas@ppb.com>
2021-10-29 21:19:17 +0100

    Implement binary search

--- src/main/java/algorithms/BinarySearch.java
+++ src/main/java/algorithms/BinarySearch.java
@@ -1,16 +1,43 @@
 package algorithms;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+
 import java.util.List;
+import java.util.Objects;
 
+@Slf4j
 public class BinarySearch {
-    public int find(List<Integer> list, Integer element) {
-        int lower = 0;
-        int upper = list.size() - 1;
-        Integer lowerElement = list.get(lower);
-        Integer upperElement = list.get(upper);
+    private static final Logger LOGGER = BinarySearch.log;
 
-        if (lowerElement.equals(element)) return lower;
-        if (upperElement.equals(element)) return upper;
+    public int find(List<Integer> list, Integer element) {
+        LOGGER.info("searching for element: {}", element);
+
+        int lower;
+        int upper;
+        int middle = list.size() / 2;
+
+        while (true) {
+            int middleEl = list.get(middle);
+            if (Objects.equals(middleEl, element)) return middle;
+            if (middleEl > element) {
+                upper = middle;
+                lower = 0;
+            } else {
+                lower = middle;
+                upper = list.size() - 1;
+            }
+            LOGGER.info("=== Upper: {}, Lower: {}, Middle: {} ===", upper, lower, middle);
+            middle = lower + upper / 2;
+            if (upper - lower <= 1) {
+                if (Objects.equals(list.get(upper), element)) {
+                    return upper;
+                } else if (Objects.equals(list.get(lower), element)) {
+                    return lower;
+                }
+                break;
+            }
+        }
         return -1;
     }
 }
--- src/test/java/algorithms/BinarySearchTest.java
+++ src/test/java/algorithms/BinarySearchTest.java
@@ -3,7 +3,6 @@ package algorithms;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Collections;
 import java.util.List;
 
 import static java.util.Collections.singletonList;
@@ -18,22 +17,37 @@ class BinarySearchTest {
     }
 
     @Test
-    public void testFind_oneElementList() {
+    public void testFind_OneElementList() {
         assertEquals(0, victim.find(singletonList(9), 9));
     }
 
     @Test
-    public void testFind_twoElementList() {
+    public void testFind_TwoElementList() {
         assertEquals(1, victim.find(List.of(2, 9), 9));
     }
 
     @Test
-    public void testFind_elementNotPresent() {
+    public void testFind_ElementNotPresent() {
         assertEquals(-1, victim.find(List.of(2, 6), 9));
     }
 
     @Test
-    public void testFind_threeElementList() {
+    public void testFind_ThreeElementList_AtElementZero() {
+        assertEquals(0, victim.find(List.of(2, 6, 9), 2));
+    }
+
+    @Test
+    public void testFind_ThreeElementList_AtElementOne() {
+        assertEquals(1, victim.find(List.of(2, 6, 9), 6));
+    }
+
+    @Test
+    public void testFind_ThreeElementList_AtElementTwo() {
         assertEquals(2, victim.find(List.of(2, 6, 9), 9));
+    }
+
+    @Test
+    public void testFind_FourElementList() {
+        assertEquals(1, victim.find(List.of(2, 6, 7, 9), 6));
     }
 }
