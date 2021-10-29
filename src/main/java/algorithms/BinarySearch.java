c6cad6fdf4676e1752811f9504874a78cb0e42bf
Adam Jonas <adam.jonas@ppb.com>
2021-10-29 23:48:54 +0100

    Simplify Binary search complexity slightly; Remove logger

--- src/main/java/algorithms/BinarySearch.java
+++ src/main/java/algorithms/BinarySearch.java
@@ -1,35 +1,23 @@
 package algorithms;
 
-import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-
 import java.util.List;
 import java.util.Objects;
 
-@Slf4j
 public class BinarySearch {
-    private static final Logger LOGGER = BinarySearch.log;
-
     public int find(List<Integer> list, Integer element) {
-        LOGGER.info("searching for element: {}", element);
-
         int lower = 0;
         int upper = list.size() - 1;
         int middle = list.size() / 2;
 
-        while (true) {
-            int middleEl = list.get(middle);
-            if (Objects.equals(middleEl, element)) return middle;
-            if (middleEl > element) upper = middle;
-            if (middleEl < element) lower = middle;
+        while (upper - lower > 1) {
+            int middleElement = list.get(middle);
+            if (Objects.equals(middleElement, element)) return middle;
+            if (middleElement > element) upper = middle;
+            if (middleElement < element) lower = middle;
             middle = (lower + upper) / 2;
-
-            if (upper - lower <= 1) {
-                if (Objects.equals(list.get(upper), element)) return upper;
-                if (Objects.equals(list.get(lower), element)) return lower;
-                break;
-            }
         }
+        if (Objects.equals(list.get(upper), element)) return upper;
+        if (Objects.equals(list.get(lower), element)) return lower;
         return -1;
     }
 }
