ad4006d9cf4f5cded11cb1cb5a8973bbb5b388fc
Adam Jonas <adam.jonas@ppb.com>
2021-10-30 12:09:38 +0100

    Implement reverse for two element list

--- src/main/java/algorithms/Reverse.java
+++ src/main/java/algorithms/Reverse.java
@@ -1,9 +1,13 @@
 package algorithms;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Reverse {
     public <T> List<T> reverse(List<T> list) {
+        if (list.size() < 2) return list;
+
+        Collections.swap(list, 0, 1);
         return list;
     }
 }
--- src/test/java/algorithms/ReverseTest.java
+++ src/test/java/algorithms/ReverseTest.java
@@ -3,6 +3,7 @@ package algorithms;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static java.util.Collections.singletonList;
@@ -17,9 +18,17 @@ class ReverseTest {
     }
 
     @Test
-    public void testReverse() {
+    public void testReverse_OneElementList() {
         List<Integer> list = singletonList(1);
 
         assertEquals(list, victim.reverse(list));
+    }
+
+    @Test
+    public void testReverse_TwoElementList() {
+        List<Integer> list = Arrays.asList(1, 2);
+        List<Integer> expected = List.of(2, 1);
+
+        assertEquals(expected, victim.reverse(list));
     }
 }
