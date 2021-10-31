ba9d3e5d1ab3349f5173bdd1d83eb859c01ce365
Adam Jonas <adam.jonas@ppb.com>
2021-10-31 21:09:10 +0000

    Implement Reverse and fully test

--- src/test/java/algorithms/ReverseTest.java
+++ src/test/java/algorithms/ReverseTest.java
@@ -6,6 +6,7 @@ import org.testng.annotations.Test;
 import java.util.Arrays;
 import java.util.List;
 
+import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
 import static org.testng.AssertJUnit.assertEquals;
 
@@ -17,6 +18,13 @@ public class ReverseTest {
     @BeforeClass
     public static void setUp() {
         victim = new Reverse();
+    }
+
+    @Test
+    public void testReverse_EmptyList() {
+        list = emptyList();
+
+        assertEquals(list, victim.reverse(list));
     }
 
     @Test
@@ -46,6 +54,30 @@ public class ReverseTest {
     public void testReverse_FourElementList() {
         list = Arrays.asList(1, 2, 3, 4);
         expected = List.of(4, 3, 2, 1);
+
+        assertEquals(expected, victim.reverse(list));
+    }
+
+    @Test
+    public void testReverse_FiveElementList() {
+        list = Arrays.asList(1, 2, 3, 4, 5);
+        expected = List.of(5, 4, 3, 2, 1);
+
+        assertEquals(expected, victim.reverse(list));
+    }
+
+    @Test
+    public void testReverse_TwelveElementList() {
+        list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
+        expected = List.of(12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1);
+
+        assertEquals(expected, victim.reverse(list));
+    }
+
+    @Test
+    public void testReverse_FifteenElementList() {
+        list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+        expected = List.of(15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1);
 
         assertEquals(expected, victim.reverse(list));
     }
