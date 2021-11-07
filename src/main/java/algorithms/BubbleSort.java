a0f2b80e14e7d11322ccace268861ca3a2d4bafe
Adam Jonas <adam.jonas@ppb.com>
2021-11-07 18:11:21 +0000

    Refactor bubbleSort, extract swap into separate method

--- src/main/java/algorithms/BubbleSort.java
+++ src/main/java/algorithms/BubbleSort.java
@@ -7,11 +7,15 @@ public class BubbleSort {
     public List<Integer> sort(List<Integer> list) {
         for (int i = 0; i < list.size() -1; i++) {
             for (int j = i + 1; j < list.size(); j++) {
-                if (list.get(i) > list.get(j)) {
-                    Collections.swap(list, i, j);
-                }
+                swap(list, i, j);
             }
         }
         return list;
+    }
+
+    private void swap(List<Integer> list, int i, int j) {
+        if (list.get(i) > list.get(j)) {
+            Collections.swap(list, i, j);
+        }
     }
 }
